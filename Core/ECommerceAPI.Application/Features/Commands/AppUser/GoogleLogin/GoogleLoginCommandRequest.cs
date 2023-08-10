212aea172456cf917507cb7c06e1b7964ca46d60
leventkalkavan <levent.kalkavan@hotmail.com>
2023-08-10 16:01:04 +0300

    googlelogin update

--- Core/ECommerceAPI.Application/Features/Commands/AppUser/GoogleLogin/GoogleLoginCommandRequest.cs
+++ Core/ECommerceAPI.Application/Features/Commands/AppUser/GoogleLogin/GoogleLoginCommandRequest.cs
@@ -8,7 +8,6 @@ public class GoogleLoginCommandRequest: IRequest<GoogleLoginCommandResponse>
     public string IdToken { get; set; }
     public string Name { get; set; }
     public string FirstName { get; set; }
-    public string LastName { get; set; }
     public string Email { get; set; }
     public string PhotoUrl { get; set; } 
     public string Provider { get; set; }
